4f135884116b2266a1a96437d472f10d26c07320
Dylan Fellows <dylanfellows990@gmail.com>
2022-03-03 23:02:32 +0800

    working prototype

--- static/javascript/java.js
+++ static/javascript/java.js
@@ -30,45 +30,100 @@ function init_game()
 
 }
 
+const goal_word = "CRANE";
+var turn_number = 0;
 
 function guess_word()
 {
-    
     var guess = document.getElementById('Guess');
     if (guess.value.length != 5)   {alert("MUST BE 5 LETTERS")}
     else
     {
-       
-        add_word(guess.value.toUpperCase(), 1);
+        turn_number++;
+        add_word(guess.value.toUpperCase(), turn_number);
 
     }
     guess.innerHTML = "";
 }
 
-function add_word(guess, turn_number)
+function add_word(guess, turn_num)
 {
     const goal_word = "CRANE";
     const used = document.getElementById("used_letters");
-    const letter1 = document.getElementById("L11");
-    const letter2 = document.getElementById("L12");
-    const letter3 = document.getElementById("L13");
-    const letter4 = document.getElementById("L14");
-    const letter5 = document.getElementById("L15");
+    const letters = [];
 
-    const letters = [letter1, letter2, letter3, letter4, letter5];
+    if(turn_num == 1)
+    {
+        
+        const letter1 = document.getElementById("L11");
+        const letter2 = document.getElementById("L12");
+        const letter3 = document.getElementById("L13");
+        const letter4 = document.getElementById("L14");
+        const letter5 = document.getElementById("L15");
+        letters.push(letter1, letter2, letter3, letter4, letter5);
+    }
+    if(turn_num == 2)
+    {
+        const letter1 = document.getElementById("L21");
+        const letter2 = document.getElementById("L22");
+        const letter3 = document.getElementById("L23");
+        const letter4 = document.getElementById("L24");
+        const letter5 = document.getElementById("L25");
+        letters.push(letter1, letter2, letter3, letter4, letter5);
+
+    }
+    if(turn_num == 3)
+    {
+        const letter1 = document.getElementById("L31");
+        const letter2 = document.getElementById("L32");
+        const letter3 = document.getElementById("L33");
+        const letter4 = document.getElementById("L34");
+        const letter5 = document.getElementById("L35");
+        letters.push(letter1, letter2, letter3, letter4, letter5);
+
+    }
+    if(turn_num == 4)
+    {
+        const letter1 = document.getElementById("L41");
+        const letter2 = document.getElementById("L42");
+        const letter3 = document.getElementById("L43");
+        const letter4 = document.getElementById("L44");
+        const letter5 = document.getElementById("L45");
+        letters.push(letter1, letter2, letter3, letter4, letter5);
+
+    }
+    if(turn_num == 5)
+    {
+        const letter1 = document.getElementById("L51");
+        const letter2 = document.getElementById("L52");
+        const letter3 = document.getElementById("L53");
+        const letter4 = document.getElementById("L54");
+        const letter5 = document.getElementById("L55");
+        letters.push(letter1, letter2, letter3, letter4, letter5);
+
+    }
+    
 
 
     for (let i = 0; i < 5; i++) 
     {
         letters[i].innerHTML += guess[i];
         
+        letters[i].style.backgroundColor = "#3a3a3c";
         if(guess[i] == goal_word[0] || guess[i] == goal_word[1] || guess[i] == goal_word[2] || guess[i] == goal_word[3] || guess[i] == goal_word[4])
         {letters[i].style.backgroundColor = "#b59f3b";}
-        if(guess[i] == goal_word[0])
+        if(guess[i] == goal_word[i])
         {letters[i].style.backgroundColor = "#538d4e";}
+
+        var letter_is_used = false;
+        for(let x = 0; x < used.innerHTML.length; x++)
+        {
+            if(guess[i] == used.innerHTML[x]) {letter_is_used = true;}
+        }
+        if(!letter_is_used) {used.innerHTML += guess[i];}
     }
 
-    used.innerHTML += guess;
+    if(guess == goal_word) {alert("WIN!!!");}
     
 
 
