3cb73bb1d15ffc4a3adb1381970db851efa86cb9
Dylan Fellows <dylanfellows990@gmail.com>
2022-03-03 18:50:53 +0800

    footer added

--- static/javascript/java.js
+++ static/javascript/java.js
@@ -9,3 +9,15 @@ function close_help()
     const help_window = document.getElementById("help_popup");
     help_window.style.display = "none";
 }
+
+function open_stats()
+{
+    const help_window = document.getElementById("stats_popup");
+    help_window.style.display = "block";  
+}
+
+function close_stats()
+{
+    const help_window = document.getElementById("stats_popup");
+    help_window.style.display = "none";
+}
