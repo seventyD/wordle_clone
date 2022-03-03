9584c33dbc694e65cb3bbcf3372d29d8f771cc64
Dylan Fellows <dylanfellows990@gmail.com>
2022-03-03 18:37:54 +0800

    added help window

---
+++ static/javascript/java.js
@@ -0,0 +1,11 @@
+function open_help()
+{
+    const help_window = document.getElementById("help_popup");
+    help_window.style.display = "block";  
+}
+
+function close_help()
+{
+    const help_window = document.getElementById("help_popup");
+    help_window.style.display = "none";
+}
